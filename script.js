e2de24ed303d6cc351101e65e28d3d9fcdecdef9
Omar Smailia <smailia.omar@iisgalvanimi.edu.it>
2025-02-07 10:49:06 +0000

    Punto 6

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 function startGame() {
     myGameArea.start()
     }
+var redSquare = {
+    width: 20,
+    height: 20,
+    x: 10,
+    y: 120,
+    color: "red"
+      };
+
 var myGameArea = {
     canvas : document.createElement("canvas"),
     start : function() {
-        this.canvas.width = 480;
-        this.canvas.height = 270;
+        this.canvas.width = 1000;
+        this.canvas.height = 500;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0])
-    }
+    },
+    draw: function(component) {
+        this.context.fillStyle = component.color;
+        this.context.fillRect(component.x, component.y, component.width, component.height);
+      }
 }
