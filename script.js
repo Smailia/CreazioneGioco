f64092c4c58f39627eabd025dcca9c072f30070d
Omar Smailia <smailia.omar@iisgalvanimi.edu.it>
2025-03-19 09:54:41 +0000

    1

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function startGame() {
     myGameArea.start()
+    myGameArea.draw(redSquare);
+    }
+    function updateGameArea() {
+      myGameArea.draw(redSquare);
     }
 var redSquare = {
     width: 20,
@@ -12,13 +16,37 @@ var redSquare = {
 var myGameArea = {
     canvas : document.createElement("canvas"),
     start : function() {
-        this.canvas.width = 1000;
+        this.canvas.width = 500;
         this.canvas.height = 500;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0])
+        this.interval = setInterval(updateGameArea, 20);
     },
     draw: function(component) {
-        this.context.fillStyle = component.color;
-        this.context.fillRect(component.x, component.y, component.width, component.height);
-      }
-}
+      this.context.fillStyle = component.color;
+      this.context.fillRect(
+          component.x, 
+          component.y, 
+          component.width, 
+          component.height
+         );
+    }
+}; 
+
+function moveup() {
+  redSquare.y -= 30;
+    }
+      
+function movedown() {
+  redSquare.y += 30;
+    }
+      
+function moveleft() {
+  redSquare.x -= 30;
+    }
+      
+function moveright() {
+  redSquare.x += 30;
+    }
+
+
